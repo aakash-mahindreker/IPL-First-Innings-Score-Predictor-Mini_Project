40325d7c5da98ddaa12b9844cc026b1df07bb97c
aakash-mahindreker <70765660+aakash-mahindreker@users.noreply.github.com>
2020-10-05 21:17:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,122 +11,20 @@ regressor = pickle.load(open(filename, 'rb'))
 app = Flask(__name__)
 
 
-def get_api_key():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config['openweathermap']['api']
-
-
-def get_weather_results(location, api_key):
-    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(location, api_key)
-    r = requests.get(api_url)
-    return r.json()
-
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/about', methods=['POST'])
+def about():
+    return render_template('about.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
+
     temp_array = list()
-    location = ""
+    
     if request.method == 'POST':
-        venue = request.form['venue']
-        if venue == 'Barabati Stadium':
-            location = "Cuttack"
-            # temp_array = temp_array + [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Brabourne Stadium':
-            location = "Mumbai"
-            # temp_array = temp_array + [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Buffalo Park':
-            location = "East London"
-            # temp_array = temp_array + [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'De Beers Diamond Oval':
-            location = "Kimberley"
-            # temp_array = temp_array + [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Dr DY Patil Sports Academy':
-            location = "Navi Mumbai"
-            # temp_array = temp_array + [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium':
-            location = "Visakhapatnam"
-            # temp_array = temp_array + [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Dubai International Cricket Stadium':
-            location = "Dubai"
-            # temp_array = temp_array + [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Eden Gardens':
-            location = "Kolkata"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Feroz Shah Kotla':
-            location = "Delhi"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Himachal Pradesh Cricket Association Stadium':
-            location = "Himachal Pradesh"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Holkar Cricket Stadium':
-            location = "Indore"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'JSCA International Stadium Complex':
-            location = "Ranchi"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Kingsmead':
-            location = "Durban"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'M Chinnaswamy Stadium':
-            location = "Bengaluru"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'MA Chidambaram Stadium, Chepauk':
-            location = "Chennai"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Maharashtra Cricket Association Stadium':
-            location = "Mumbai"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'New Wanderers Stadium':
-            location = "Sandton"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Newlands':
-            location = "Cape Town"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'OUTsurance Oval':
-            location = "Bloemfontein"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Punjab Cricket Association IS Bindra Stadium, Mohali':
-            location = "Punjab"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Punjab Cricket Association Stadium, Mohali':
-            location = "Punjab"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Rajiv Gandhi International Stadium, Uppal':
-            location = "Hyderabad"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]
-        elif venue == 'Sardar Patel Stadium, Motera':
-            location = "Ahmedabad"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]
-        elif venue == 'Sawai Mansingh Stadium':
-            location = "Jaipur"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]
-        elif venue == 'Shaheed Veer Narayan Singh International Stadium':
-            location = "Raipur"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0]
-        elif venue == 'Sharjah Cricket Stadium':
-            location = "Sharjah"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0]
-        elif venue == 'Sheikh Zayed Stadium':
-            location = "Abu Dhabi"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0]
-        elif venue == "St George's Park":
-            location = "Tatenhill"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]
-        elif venue == 'Subrata Roy Sahara Stadium':
-            location = "Mumbai"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]
-        elif venue == 'SuperSport Park':
-            location = "Gauteng"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]
-        elif venue == 'Wankhede Stadium':
-            location = "Mumbai"
-            # temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
         batting_team = request.form['batting-team']
         if batting_team == 'Chennai Super Kings':
             temp_array = temp_array + [1, 0, 0, 0, 0, 0, 0, 0]
@@ -161,14 +59,15 @@ def predict():
             temp_array = temp_array + [0, 0, 0, 0, 0, 0, 1, 0]
         elif bowling_team == 'Sunrisers Hyderabad':
             temp_array = temp_array + [0, 0, 0, 0, 0, 0, 0, 1]
-        city = location
         overs = request.form['overs']
         runs = request.form['runs']
         wickets = request.form['wickets']
         runs_in_prev_5 = request.form['runs_in_prev_5']
         wickets_in_prev_5 = request.form['wickets_in_prev_5']
-        if venue == "None" or batting_team == "None" or bowling_team == "None" or location == "" or runs is None or overs is None or wickets is None or runs_in_prev_5 is None or wickets_in_prev_5 is None:
+
+        if batting_team == "None" or bowling_team == "None" or  runs is None or overs is None or wickets is None or runs_in_prev_5 is None or wickets_in_prev_5 is None:
             return render_template('result1.html')
+
         overs = float(overs)
         runs = int(runs)
         wickets = int(wickets)
@@ -177,23 +76,17 @@ def predict():
 
         if batting_team == bowling_team or overs < 5 or overs > 20 or (overs * 10) % 10 >= 6 or wickets > 20 or runs < runs_in_prev_5 or wickets < wickets_in_prev_5:
             return render_template('result1.html')
-        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
+        if overs==5 and runs!=runs_in_prev_5:
+            return render_template('result1.html')
 
+        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
         data = np.array([temp_array])
         my_prediction = int(regressor.predict(data)[0])
 
-        api_key = get_api_key()
-        data = get_weather_results(city, api_key)
-        temp = "{0:.2f}".format(data["main"]["temp"])
-        feels_like = "{0:.2f}".format(data["main"]["feels_like"])
-        weather = data["weather"][0]["main"]
-        Location = data["name"]
+        if overs == 20 or overs == 5 or wickets==10:
+            my_prediction = runs
 
-        if overs == 20 or overs == 5:
-            my_prediction = (runs + my_prediction) // 2
-
-        return render_template('result.html', location=Location, temp=temp, feels_like=feels_like, weather=weather,
-                               result=my_prediction)
+        return render_template('result.html', result=my_prediction)
 
 
 if __name__ == '__main__':
